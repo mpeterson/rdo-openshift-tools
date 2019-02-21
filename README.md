f67539294a0aaca9ccf41f893c04b4dc8788b56a
Michel Peterson <michel@redhat.com>
2019-02-21 16:28:27 +0200

    Add tool to install OpenShift on RDO

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # rdo-openshift-tools
-A repo with tools to run OpenShift on RDO
+A repo with tools to run OpenShift on RDO (or any other Openstack based cloud)
+
+## rdo-install-openshift.sh
+
+### Usage
+
+#### Prerequisities
+
+Your RDO cloud must meet the following requirements:
+
+* An internal network configured
+* An external network configured
+* A router routing between the external and internal networks
+* An SSH key configured
+* OPTIONAL: A security group with INGRESS TCP/22 from 0.0.0.0/0
+
+Additionally, your local machine must meet the following requirements:
+
+* Have the openstack client installed:
+
+```
+sudo pip install -U python-openstackclient
+```
+
+* Have the RDO cloud Openstack RC file sourced on the prompt where the script will be run.
+
+
+#### Automatic
+
+1. Source your openstackrc
+
+```
+. openstackrc
+```
+
+2. Run the following command. Don't forget to modify the OKD_USER and OKD_PASS
+
+```
+curl https://raw.githubusercontent.com/mpeterson/rdo-openshift-tools/master/rdo-install-openshift.sh | OKD_USER=myuser OKD_PASS=mypass /bin/bash
+```
+
+#### Manual
+
+1. Clone the repository
+
+```
+git clone https://github.com/mpeterson/rdo-openshift-tools
+```
+
+2. Define mandatory variables for the installation process
+
+```
+# User created after installation
+export OKD_USER=myuser
+
+# Password for the user
+export OKD_PASS=mypass
+```
+
+3. Source your openstackrc
+
+```
+. openstackrc
+```
+
+4. Run script
+
+```
+./rdo-install-openshift.sh
+```
+
+Optionally a name for the server to be created can be provided:
+
+```
+./rdo-install-openshift.sh my-custom-server-name
+```
+
+### Options
+
+The following environmental variables can be provided to customize the image creation:
+
+* `OKD_DOMAIN`: Domain to be used for the OpenShift install.
+* `OKD_USER`: User created after installation.
+* `OKD_PASS`: Password for the user.
+* `SERVER_NAME`: Server name for the Openstack nova instance.
+* `OS_IMAGE`: Openstack ID of the image that will be used to create the nova instance.
+* `OS_FLAVOR`: Openstack ID of the nova flavor.
+* `OS_NETID`: Openstack ID of the internal network to be attached.
+* `OS_EXTNETID`: Openstack ID of the external network to be attached.
+* `OS_FIP`: Openstack ID of the FIP to be attached.
+* `OS_SECGRP`: Openstack ID of the security group to be attached.
+* `OS_KEYPAIR`: Openstack name of the SSH key to be used.
---
+++ rdo-install-openshift.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+if [ -z "$OS_AUTH_URL" ]; then
+    echo "ERROR: Please source your Openstack RC file"
+    exit 1
+fi
+
+if [ -z "$OKD_USER" ] || [ -z "$OKD_PASS" ]; then
+   echo "ERROR: OKD_USER and OKD_PASS should be set to create the initial OpenShift user."
+   exit 1
+fi
+
+echo "Preparing for server creation..."
+export SERVER_NAME=${1:-"okd-$(shuf -n1 /usr/share/dict/words|tr '[:upper:]' '[:lower:]')"}
+export OS_IMAGE=${OS_IMAGE:-fdfd5d39-a76d-45df-abec-17f768ba3054}
+export OS_FLAVOR=${OS_FLAVOR:-m1.large2}
+
+export OS_NETID=${OS_NETID:-$(openstack network list --internal -f value -c ID| head -n 1)}
+export OS_EXTNETID=${OS_EXTNETID:-$(openstack network list --external -f value -c ID| head -n 1)}
+export OS_FIP=$(openstack floating ip list --status DOWN -f value -c 'Floating IP Address'|head -n1)
+export OS_SECGRP=${OS_SECGRP:-$(openstack security group rule list --ingress --protocol tcp -f value |awk '/0.0.0.0\/0/ {split($4,port,":"); if(22>=port[1] && 22<=port[2]){print $6}}'|head -n 1)}
+export OS_KEYPAIR=${OS_KEYPAIR:-$(openstack keypair list -f value -c Name|head -n 1)}
+
+
+if [ -z "$OS_NETID" ] || [ -z "$OS_EXTNETID" ]; then
+   echo "ERROR: Internal or External networks are not configured properly."
+   exit 1
+fi
+
+if [ -z "$OS_KEYPAIR" ]; then
+   echo "ERROR: Could not find a SSH key to provide you SSH access with."
+   exit 1
+fi
+
+if [ -z "$OS_FIP" ]; then
+    export OS_FIP=$(openstack floating ip create $OS_EXTNETID -f value -c floating_ip_address)
+
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Could not create a FIP to be used."
+        exit 1
+    fi
+fi
+
+
+cat > /tmp/cloud-install-openshift.sh << EOFCLOUD
+#!/bin/bash
+
+yum install -y tmux
+
+cd /home/centos
+
+cat > provision-openshift.sh <<EOF
+#!/bin/bash
+export DOMAIN=${OKD_DOMAIN:-$OS_FIP.nip.io}
+export USERNAME=$OKD_USER
+export PASSWORD=$OKD_PASS
+curl https://raw.githubusercontent.com/gshipley/installcentos/master/install-openshift.sh | INTERACTIVE=false sudo -E bash
+EOF
+
+chown centos:centos provision-openshift.sh
+chmod +x provision-openshift.sh
+
+sudo -u centos tmux new-session -d -s install-openshift 'bash ./provision-openshift.sh 2>&1|tee provision-openshift.log '
+EOFCLOUD
+
+chmod +x /tmp/cloud-install-openshift.sh
+
+OPT_SECGRP="--security-group default"
+if [ ! -z "$OS_SECGRP" ]; then
+    OPT_SECGRP+=" --security-group $OS_SECGRP"
+fi
+
+echo "Creating server..."
+openstack server create --flavor $OS_FLAVOR $OPT_SECGRP --key-name "$OS_KEYPAIR" --image $OS_IMAGE --nic net-id=$OS_NETID --user-data /tmp/cloud-install-openshift.sh $SERVER_NAME > /tmp/$SERVER_NAME-creation.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: Server could not be created. View logs at /tmp/$SERVER_NAME-creation.log"
+    exit 1
+fi
+
+echo "Assigning FIP..."
+openstack server add floating ip $SERVER_NAME $OS_FIP > /tmp/$SERVER_NAME-fip.log 2>&1
+if [ $? -ne 0 ]; then
+    echo "ERROR: Server could not be assigned a FIP. View logs at /tmp/$SERVER_NAME-fip.log"
+    exit 1
+fi
+
+
+
+echo
+echo "Server created with the name: $SERVER_NAME"
+echo
+
+SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+MSG="can access your server by ssh-ing into it:
+
+    ssh $SSH_OPTS centos@$OS_FIP"
+
+if [ -z "$OS_SECGRP" ]; then
+    echo "There was no Security Group with SSH access. Please create one, assign it to this server and then you $MSG"
+    echo
+    echo 'This can be done by executing:'
+    echo
+    echo "openstack server add security group $SERVER_NAME \$(openstack security group rule create --dst-port 22 -f value -c id \$(openstack security group create 'SSH' -f value -c id))"
+else
+    echo "You $MSG"
+fi
+
+echo
+echo 'To view the tmux of the openshift installation run:
+
+    tmux a -t install-openshift
+
+It will take approximate 30 minutes to finish the installation'
